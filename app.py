8eb58c352155a7cd4006c34d1a20aea4dada9dd8
Breyanna Vass <breyannavass@gmail.com>
2020-01-22 14:05:49 -0500

    implemented a message

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ def submit():
         dealer = request.form['dealer']
         rating = request.form['rating']
         comments = request.form['comments']
-        print(customer, dealer, rating, comments)
+        # print(customer, dealer, rating, comments)
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='Please enter required fields')
         return render_template('success.html')
 
 
