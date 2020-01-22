55bddb16e409efe574df9c5b870cd7f4e643c06a
Breyanna Vass <breyannavass@gmail.com>
2020-01-22 14:02:05 -0500

    fixed some wording

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        print(customer, dealer, rating, comments)
+        return render_template('success.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
